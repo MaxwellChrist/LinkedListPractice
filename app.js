643515c549ace5902d729a5e9bda6269930c54d8
MaxwellChrist <ribthulu@gmail.com>
2022-08-09 12:08:41 -0400

    more practice with linked lists

--- app.js
+++ app.js
@@ -92,7 +92,6 @@ newList.insertAfterValue(4, 6)
 newList.print()
 newList.deleteByValue(2)
 newList.print()
-
 ///////The tutorial below is where I'm exploring creating a list node with a function instead of a class///////////////
 function createMeANode(value) {
     return {
@@ -132,6 +131,119 @@ class LinkedList2 {
 
 const myOtherLinkedList = new LinkedList2
 myOtherLinkedList.insert(5)
-// myOtherLinkedList.insert("Hello, my name is Max")
-// myOtherLinkedList.insert(false)
+myOtherLinkedList.insert("Hello, my name is Max")
+myOtherLinkedList.insert(false)
 myOtherLinkedList.print()
+////////////////////More practice with singly linked lists//////////////////////////////////
+class Node {
+    constructor(data, next = null) {
+        this.data = data
+        this.next = next
+    }
+}
+
+const n1 = new Node(100)
+console.log(n1)
+
+class LinkedList3 {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+    insertFirst(data) {
+        this.head = new Node(data, this.head)
+        this.size++
+    }
+    insertLast(data) {
+        let node = new Node(data)
+        let current
+        if (this.head === null) {
+            this.head === node
+        } else {
+            current = this.head
+            while(current.next !== null) {
+                current = current.next
+            }
+            current.next = node
+
+        }
+        this.size++
+    }
+    insertAt(data, index) {
+        if (index > 0 && index > this.size) {
+            return
+        }
+        if (index === 0) {
+            return this.insertFirst(data)
+        }
+
+        const node = new Node(data)
+        let previous
+        let current = this.head
+        let count = 0 
+        while(count < index) {
+            previous = current
+            current = current.next
+            count++
+        }
+        node.next = current
+        previous.next = node
+        this.size++
+    }
+    getAt(index) {
+        let current = this.head
+        let count = 0
+        while(current) {
+            if(count == index) {
+                console.log(current.data)
+            }
+            count++
+            current = current.next
+        }
+        return null
+    }
+    removeAt(index) {
+        if (index > 0 && index > this.size) {
+            return
+        }
+        let current = this.head
+        let previous
+        let count = 0
+        if (index === 0) {
+            this.head = current.next
+        } else {
+            while(count < index) {
+                previous = current
+                current = current.next
+                count++
+            }
+            previous.next = current.next
+        }
+        this.size--
+    }
+    printListData() {
+        let current = this.head
+        while(current) {
+            console.log(current.data)
+            current = current.next
+        }
+    }
+    clear() {
+        this.head = null
+        this.size = 0
+    }
+}
+
+const ll = new LinkedList3
+ll.insertFirst(1)
+ll.insertFirst(2)
+ll.insertFirst(3)
+ll.clear()
+ll.insertLast(4)
+ll.insertAt(5 ,2)
+ll.insertAt(6 , 0)
+ll.insertAt(10, 10)
+ll.getAt(0)
+ll.getAt(20)
+ll.removeAt(0)
+ll.printListData()
